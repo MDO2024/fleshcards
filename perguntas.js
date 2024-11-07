c80397f93c6cae29765d70b9b4cb1b7e1fcea6d6
MDO2024 <00001088355869sp@al.educacao.sp.gov.br>
2024-11-07 20:37:02 -0300

    CardsAlura

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Jurassic Park',
+    'Quem criou o parque dos dinossauros em Jurassic Park?',
+    'John Hammond, fundador da InGen, é o criador do parque'
+)
+
+criaCartao(
+    'Jurassic Park',
+    'Como os dinossauros foram recriados em Jurassic Park?',
+    'Através da clonagem, usando DNA de dinossauros extraído de mosquitos fossilizados em âmbar'
+)
+
+criaCartao(
+    'Jurassic Park',
+    'Qual dinossauro é o principal antagonista no primeiro filme?',
+    'O Tyrannosaurus rex e os Velociraptors são os principais antagonistas dinossauros'
+)
+
+criaCartao(
+    'Jurassic Park',
+    'Qual é a famosa frase de Ian Malcolm em Jurassic Park?',
+    '"A vida encontra um meio."'
+)
